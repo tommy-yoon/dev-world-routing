677ed78844eaaa532ba9fe464cc26bd669ac12c2
tommy-yoon <taeil.yoon@gmail.com>
2021-11-03 14:09:33 +1300

    add Country and finish the basic features

---
+++ client/components/Country.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
+
+import countries from '../../data/countries'
+
+function Country () {
+  const { name } = useParams()
+  console.log('Country', 'name', name)
+  const { code } = useParams()
+  console.log('Country', 'code', code)
+
+  const country = countries.find(ele => ele.code === code)
+
+  return (
+    <div className="country">
+      <h1>{country.flag} {country.name}</h1>
+      <div className='description'>
+        <p className='bold'>Capital:</p>&nbsp;<p>{country.capital}</p>
+      </div>
+      <div className='description'>
+        <p className='bold'>Area:</p>&nbsp;<p>{country.areaSqKms} km&sup2;</p>
+      </div>
+      <div className='description'>
+        <p className='bold'>Population:</p>&nbsp;<p>{country.population}</p>
+      </div>
+      <div className='description'>
+        <p className='bold'>Currency:</p>&nbsp;<p>{country.currencyName}({country.currencyCode})</p>
+      </div>
+      <div className='description'>
+        <p className='bold'>Neighbours:</p>
+        <ul>
+          {country.neighbours.split(',').map((code) => {
+            const country = countries.find(ele => ele.code === code)
+            return <li key={code}>
+              <Link to={`/continent/${name}/${code}`}>{country.name}</Link>
+            </li>
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Country
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+
 import { Link } from 'react-router-dom'
+import { useParams } from 'react-router'
 
 import continents from '../../data/continents'
 
 function Nav () {
+  const { name } = useParams()
+  console.log('In Nav', 'name', name)
   return (
     <div>
       <h2>Nav</h2>
+      <Link to='/'>Home</Link>
       <ul>
         {
           Object.keys(continents).map(continent => {
-            return <li key={continent}>
-              <Link to={'/continents/' + continent}>{continent}</Link>
+            return <li key={continent} className={continent === name ? 'selected' : ''}>
+              <Link to={`/continents/${continent}`}>{continent}</Link>
             </li>
           })}
       </ul>
